f364d26c8bbb78ffc9294a75b3c42e603afb2caf
jesseRG1 <jesse_rg1@hotmail.com>
2024-03-20 11:14:08 -0300

    ejercicio 4 error

---
+++ firmware/projects/Guia1_ej4/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/Jesse/esp/v5.1.2/esp-idf/components/bootloader/subproject"
+  "C:/Repositorios/RojasGarrido-1C-2024/firmware/projects/Guia1_ej4/build/bootloader"
+  "C:/Repositorios/RojasGarrido-1C-2024/firmware/projects/Guia1_ej4/build/bootloader-prefix"
+  "C:/Repositorios/RojasGarrido-1C-2024/firmware/projects/Guia1_ej4/build/bootloader-prefix/tmp"
+  "C:/Repositorios/RojasGarrido-1C-2024/firmware/projects/Guia1_ej4/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Repositorios/RojasGarrido-1C-2024/firmware/projects/Guia1_ej4/build/bootloader-prefix/src"
+  "C:/Repositorios/RojasGarrido-1C-2024/firmware/projects/Guia1_ej4/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Repositorios/RojasGarrido-1C-2024/firmware/projects/Guia1_ej4/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Repositorios/RojasGarrido-1C-2024/firmware/projects/Guia1_ej4/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ firmware/projects/Guia1_ej4/main/Guia1_ej4.c
@@ -0,0 +1,56 @@
+/*! @mainpage Template
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 20/03/2024 | ejercicio 4 guia 4                             |
+ *
+ * @author Jessenia Jazmin Rojas Garrido (jrojasgarrido@ingenieria.uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+uint8_t CONT;
+uint8_t vect[3];
+uint8_t i;
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+int8_t  convertToBcdArray (uint32_t data, uint8_t digits, uint8_t * bcd_number)
+{
+	for(CONT=0; CONT<digits; CONT++){
+		bcd_number [CONT] = data%10;
+		data = data/10;
+	}
+	return 1;
+}
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	
+	convertToBcdArray (138,3,vect[3]);
+	for (i=0;i<3;i++)
+	{
+		printf("nº: %d en posición %d\n", vect[i], i);
+	}
+
+	
+}
+/*==================[end of file]============================================*/
