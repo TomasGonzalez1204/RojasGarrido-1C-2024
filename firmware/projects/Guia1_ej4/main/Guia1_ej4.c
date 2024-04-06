cf3ca164ffb407256209615639eeac48e6664ed1
jesseRG1 <jesse_rg1@hotmail.com>
2024-04-06 20:53:32 -0300

    ejercicio 5 terminado

--- firmware/projects/Guia1_ej4/main/Guia1_ej4.c
+++ firmware/projects/Guia1_ej4/main/Guia1_ej4.c
@@ -29,13 +29,20 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "led.h"
+#include "gpio_mcu.h"
 /*==================[macros and definitions]=================================*/
+
 uint8_t CONT;
 uint8_t vect[3];
 uint8_t i;
 #define CONFIG_BLINK_PERIOD 1000
+typedef struct
+{
+	gpio_t pin;			/*!< GPIO pin number */
+	io_t dir;			/*!< GPIO direction '0' IN;  '1' OUT*/
+}  gpioConf_t;
 /*==================[internal data definition]===============================*/
-
+gpioConf_t arreglo [4] = {{GPIO_20,GPIO_OUTPUT},{GPIO_21,GPIO_OUTPUT},{GPIO_22,GPIO_OUTPUT},{GPIO_23,GPIO_OUTPUT}};
 /*==================[internal functions declaration]=========================*/
 int8_t  convertToBcdArray (uint32_t data, uint8_t digits, uint8_t * bcd_number)
 {
@@ -46,23 +53,36 @@ int8_t  convertToBcdArray (uint32_t data, uint8_t digits, uint8_t * bcd_number)
 	return 1;
 }
 
-/*==================[external functions definition]==========================*/
-void app_main(void){
-	
-	convertToBcdArray (138,3,vect);
-	for (i=0;i<3;i++)
+	void BCDtoGPIO(gpioConf_t* array,uint8_t digito)
 	{
-		printf("nº: %d en posición %d\n", vect[i], i);
+		uint8_t mascara=1;
+		for (uint8_t i = 0; i < 4; i++)
+		{
+			GPIOInit(arreglo[i].pin, arreglo[i].dir);
+		}
+		for (uint8_t j = 0; j < 4; j++)
+		{
+			if ((mascara&digito) == 0)
+			{
+				GPIOOff(array[j].pin);
+				//printf("SE PONE EN BAJO PIN ",array[j].pin);
+			} else 
+			{
+				GPIOOn(array[j].pin);
+				//printf("SE PONE EN ALTO PIN ",array[j].pin);
+			}
+		mascara = mascara << 1;	
+		}
 	}
 
-	 while(true){
-        printf("LED ON\n");
-       // LedOn(LED_1);
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
-        printf("LED OFF\n");
-       // LedOff(LED_1);
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
-    }
 	
+/*==================[external functions definition]==========================*/
+void app_main(void)
+{
+	BCDtoGPIO(arreglo,2);
+	convertToBcdArray (138,3,vect);
+	for (i=0;i<3;i++){
+		printf("nº: %d en posición %d\n", vect[i], i);
+	}
 }
 /*==================[end of file]============================================*/
--- firmware/projects/Guia1_ej5/main/Guia1_ej5.c
+++ firmware/projects/Guia1_ej5/main/Guia1_ej5.c
@@ -17,23 +17,46 @@
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 3/04/2024 | Document creation		                         |
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Jessenia J. Rojas Garrido (Jrojasgarrido@ingenieria.uner.edu.ar)
  *
  */
 
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+#include "gpio_mcu.h"
 /*==================[macros and definitions]=================================*/
+
+
+typedef struct
+{
+	gpio_t pin;			/*!< GPIO pin number */
+	io_t dir;			/*!< GPIO direction '0' IN;  '1' OUT*/
+}  gpioConf_t;
 
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
-
+void BCDtoGPIO(gpioConf_t array,uint8_t digito){
+	uint8_t mascara=1;
+	for (uint8_t i = 0; i < 4; i++){
+		GPIOInit(array[i].pin, array[i].dir);
+	}
+	for (uint8_t j = 0; j < 4; j++)
+	
+	{
+		if (mascara & digito ==0)
+		{
+			GPIOOff(array[i].pin);
+		} else {GPIOOn(array[i].pin)};
+	}
+	mascara = mascara << 1;	
+	}
+}
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	BCDtoGPIO ;
 }
 /*==================[end of file]============================================*/
