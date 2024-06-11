23d815bacd6aefa6e182c028542bf5442c56880a
jesseRG1 <jesse_rg1@hotmail.com>
2024-06-11 13:07:53 -0300

    tareas creadas

--- firmware/projects/Examen/main/Examen.c
+++ firmware/projects/Examen/main/Examen.c
@@ -39,28 +39,37 @@ typedef struct
 }  gpioConf_t;
 /*==================[internal data definition]===============================*/
 gpioConf_t arreglo [3] = {{GPIO_20,GPIO_OUTPUT},{GPIO_21,GPIO_OUTPUT},{GPIO_22,GPIO_OUTPUT}};// (agua, PHA,PHB)
-uint8_t entrada=0,pH=0,banderaPHA=0,banderaPHB;
+uint8_t pH=0,banderaAgua,banderaPHA=0,banderaPHB;
+TaskHandle_t bombaPH_task_handle, bombaAgua_task_handle;
 #define CONFIG_BLINK_PERIOD_MEDICION 3000000;
+
 /*==================[internal functions declaration]=========================*/
 void EncenderBomba(gpioConf_t* Bomba,uint8_t Bandera)
 {
-	while(1){
 		if(Bandera==1) 
-			GPIOOn(Bomba);
-		else GPIOOff (Bomba);
-	vTaskDelay (CONFIG_BLINK_PERIOD/portTICK_PERIOD_MS);
-	}
+			GPIOOn(Bomba.pin);
+		else GPIOOff (Bomba.pin);
 }
 
-void bombaPH (void *param)
+void bombaAgua (uint8_t entrada)
+{
+	EncenderBomba(arreglo[0],BanderaAgua)
+}
+
+void bombaPH (uint8_t entrada)
 {
 	while(1)
 	{
 		pH=entrada*(3/14);
-		if(pH<14 && pH>6.7){
-			EncenderBomba(arreglo[1],banderaPHA);}
-		else if(pH<6 && pH>0){
-			EncenderBomba(arreglo[2],banderaPHB);}
+		if(pH<14 && pH>6.7)
+		{
+			EncenderBomba(arreglo[1].pin,banderaPHA);
+		}
+		else if(pH<6 && pH>0)
+			{
+				EncenderBomba(arreglo[2].pin,banderaPHB);
+			}
+	vTaskDelay (CONFIG_BLINK_PERIOD_MEDICION / portTICK_PERIOD_MS);
 	}
 }
 /*==================[external functions definition]==========================*/
@@ -70,7 +79,8 @@ void app_main(void){
     GPIOInit(Bombas[0].pin,Bombas[0].dir);
 	GPIOInit(Bombas[1].pin,Bombas[1].dir);
 	GPIOInit(Bombas[2].pin,Bombas[2].dir);
-
-
+	xTaskCreate(&bombaPH, "bombaPH", 512, NULL, 5, &bombaPH_task_handle);
+	xTaskCreate(&bombaAgua, "bombaAgua", 512, NULL, 5, &bombaAgua_task_handle);
+	
 }
 /*==================[end of file]============================================*/
